d758eeb95fbb4e3eaf942f7cf6bc63153fe835de
Geoffrey Kirk <Geoffrey_Kirk@Trimble.com>
2019-02-17 10:31:23 -0700

    Make the page look nicer

--- cgi/List_Status.php
+++ cgi/List_Status.php
@@ -2,14 +2,28 @@
 <head>
 <title>GNSS Receivers</title>
 <link rel="stylesheet" type="text/css" href="/style.css"></link>
+<link rel="stylesheet" type="text/css" href="/css/tcui-styles.css">
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script src="/jquery.tablesorter.min.js"></script>
+<body class="page">
+<div class="container clearfix">
+  <div style="padding: 10px 10px 10px 0 ;"> <a href="http://construction.trimble.com/">
+        <img src="/images/trimble-logo.png" alt="Trimble Logo" id="logo"> </a>
+      </div>
+  <!-- end #logo-area -->
+</div>
+<div id="top-header-trim"></div>
+<div id="content-area">
+<div id="content">
+<div id="main-content" class="clearfix">
+
+
 
 <style>
 
 table.tablesorter tbody td.Issue {
     color: red;
-} 
+}
 </style>
 <script>
 $(document).ready(function()
@@ -116,7 +130,7 @@ else {
 
        if ($row["Checked"] ){
            echo "\n<td> ". $row["Last_Check"]." </td>";
-           } 
+           }
        else {
           echo "\n<td> Disabled </td>";
           }
@@ -326,3 +340,9 @@ if ($_REQUEST["User_ID"]) {
     }
 ?>
 </form>
+
+</div>
+</div>
+</div>
+
+</html>
