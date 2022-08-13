168fbbab62903cd5e0b623b5225848338f015a1b
Geoffrey Kirk <GeoffreyKirk@Gmail.com>
2022-08-13 12:08:26 -0600

    First pass at really simple setup
    Got sick of manually copying updates...

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+WWW=/var/www/html/Dashboard
+CGI=/use/lib/cgi-bin/Dashboard
+WWW_USER=www-data
+
+
+mkdir -p $WWW || exit Error could not make directory $WWW
+mkdir -p $WWW/Firmware || exit Error could not make directory $WWW/Firmware
+mkdir -p $WWW/Clones || exit Error could not make directory $WWW/Clones
+
+mkdir -p $CGI || exit Error could not make directory $CGI
+mkdir -p $CGI/User || exit Error could not make directory $CGI/User
+
+cp  cgi $CGI
+cp  www $WWW
+
+cd $CGI
+chown $WWW_USER $WWW/*
+chown $WWW_USER $CGI/*
+chmod +x $CGI/*
+
+
+
+chown $WWW_USER $WWW $WWW/Firmware $WWW/Clones $CGI
+
+cd $CGI
+
+sudo php ./DB_Setup.php
+
+if [ =f gnss.db ]
+then
+    echo "Error: gnss.db not created. PHP installed?"
+fi
+
+chown $WWW_USER gnss.db
+
