aaa6db275ece74703e6ed2abc2535cafd63a2d91
Geoffrey Kirk <Geoffrey_Kirk@Trimble.com>
2022-08-16 21:13:43 -0600

    Add BX992-SPS

--- www/Edit_GNSS.php
+++ www/Edit_GNSS.php
@@ -147,17 +147,18 @@ Receiver:
 </td><td>
 <select required name=Receiver_Type>
   <option value="118" <?php echo ($row["Reciever_Type"]=="118")?"selected":"" ?> <?php echo ($Editing?"":"selected") ?>>SPS855</option>
-  <option value="107" <?php echo ($row["Reciever_Type"]=="107")?"selected":"" ?> >SPS852</option>
   <option value="250" <?php echo ($row["Reciever_Type"]=="250")?"selected":"" ?>>SPS585</option>
+  <option value="107" <?php echo ($row["Reciever_Type"]=="107")?"selected":"" ?>>SPS852</option>
   <option value="101" <?php echo ($row["Reciever_Type"]=="101")?"selected":"" ?>>SPS985</option>
   <option value="169" <?php echo ($row["Reciever_Type"]=="169")?"selected":"" ?>>SPS986</option>
   <option value="138" <?php echo ($row["Reciever_Type"]=="138")?"selected":"" ?>>SPS356</option>
-  <option value="38" <?php echo ($row["Reciever_Type"]=="38")?"selected":"" ?> >SPS850</option>
-  <option value="59" <?php echo ($row["Reciever_Type"]=="59")?"selected":"" ?> >SPS851</option>
-  <option value="240" <?php echo ($row["Reciever_Type"]=="240")?"selected":"" ?> >BD935</option>
-  <option value="100" <?php echo ($row["Reciever_Type"]=="100")?"selected":"" ?> >R10</option>
-  <option value="188" <?php echo ($row["Reciever_Type"]=="188")?"selected":"" ?> >R750</option>
-  <option value="76" <?php echo ($row["Reciever_Type"]=="76")?"selected":"" ?> >NetR9</option>
+  <option value="38"  <?php echo  ($row["Reciever_Type"]=="38")?"selected":"" ?>>SPS850</option>
+  <option value="59"  <?php echo  ($row["Reciever_Type"]=="59")?"selected":"" ?>>SPS851</option>
+  <option value="240" <?php echo ($row["Reciever_Type"]=="240")?"selected":"" ?>>BD935</option>
+  <option value="100" <?php echo ($row["Reciever_Type"]=="100")?"selected":"" ?>>R10</option>
+  <option value="188" <?php echo ($row["Reciever_Type"]=="188")?"selected":"" ?>>R750</option>
+  <option value="76"  <?php echo  ($row["Reciever_Type"]=="76")?"selected":"" ?>>NetR9</option>
+  <option value="509" <?php echo ($row["Reciever_Type"]=="509")?"selected":"" ?>>BX992-SPS</option>
 </select>
 </td></tr>
 
@@ -375,7 +376,7 @@ Enabled:
 <tr><td>
 FTP To:
 </td><td>
-<input name="FTP_To" type="text" size="40" value=" <?php echo ($Editing?$row["FTP_To"]:"/TCC/BTN/gnsstransfer") ?>"/>
+<input name="FTP_To" type="text" size="40" value="<?php echo ($Editing?$row["FTP_To"]:"/TCC/BTN/gnsstransfer") ?>"/>
 </td></tr>
 </table>
 
