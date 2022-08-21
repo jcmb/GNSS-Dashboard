da601395aa21ab4792dcf820c7a7d8e09a7d5060
Geoffrey Kirk <Geoffrey_Kirk@Trimble.com>
2022-08-21 09:48:26 -0600

    Add cgiDir and wwwDir

--- cgi/db_inc.py
+++ cgi/db_inc.py
@@ -1,5 +1,11 @@
+def cgiDir():
+   return "/usr/lib/cgi-bin/Dashboard/";
+
+def wwwDir():
+   return "/var/www/html/Dashboard/";
+
 def databaseFile():
-   return "/usr/lib/cgi-bin/Dashboard/GNSS.db";
+   return cgiDir() + "GNSS.db";
 
 def firmwareLocation():
    return "/var/www/html/Dashboard/Firmware";
